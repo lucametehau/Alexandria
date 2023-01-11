5c356ca379ddbb5d96f59b18a7595a7e0f7dbb9d
PGG106 <g.pagano45@studenti.unisa.it>
2023-01-11 11:36:40 +0100

    take 3
    Bench: 5167924

--- src/History.cpp
+++ src/History.cpp
@@ -12,12 +12,12 @@ void updateHH(const S_Board* pos, Search_data* sd, const int depth, const int be
 		bonus = bonus - getHHScore(pos, sd, move) * abs(bonus) / 32768;
 		//We increase the score for the bestmove
 		if (move == bestmove) {
-			sd->searchHistory[pos->side][pos->pieces[From(bestmove)]]
+			sd->searchHistory[pos->side][From(bestmove)]
 				[To(bestmove)] += bonus;
 		}
 		// and decrease it for all the others
 		else {
-			sd->searchHistory[pos->side][pos->pieces[From(move)]]
+			sd->searchHistory[pos->side][From(move)]
 				[To(move)] -= bonus;
 		}
 	}
@@ -25,12 +25,12 @@ void updateHH(const S_Board* pos, Search_data* sd, const int depth, const int be
 
 //Returns the history score of a move
 int getHHScore(const S_Board* pos, const Search_data* sd, const int  move) {
-	return sd->searchHistory[pos->side][pos->pieces[From(move)]][To(move)];
+	return sd->searchHistory[pos->side][From(move)][To(move)];
 }
 //Resets the history table
 void cleanHistory(Search_data* ss) {
 	//For every piece [12] moved to every square [64] we reset the searchHistory value
-	for (int index = 0; index < 12; ++index) {
+	for (int index = 0; index < 64; ++index) {
 		for (int index2 = 0; index2 < 64; ++index2) {
 			ss->searchHistory[0][index][index2] = 0;
 			ss->searchHistory[1][index][index2] = 0;
