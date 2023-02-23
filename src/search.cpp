de35492252edc1a5d523d077cda2a2b761807b83
PGG106 <g.pagano45@studenti.unisa.it>
2023-02-23 11:57:54 +0100

    make lmp more aggressive Score of Alexandria-dev vs Alexandria-3.5: 373 - 273 - 830  [0.534] ...      Alexandria-dev playing White: 339 - 15 - 384  [0.720] 738 ...      Alexandria-dev playing Black: 34 - 258 - 446  [0.348] 738 ...      White vs Black: 597 - 49 - 830  [0.686] 1476 Elo difference: 23.6 +/- 11.7, LOS: 100.0 %, DrawRatio: 56.2 % SPRT: llr 2.97 (100.7%), lbound -2.94, ubound 2.94 - H1 was accepted Bench: 4470804

--- src/search.cpp
+++ src/search.cpp
@@ -518,7 +518,7 @@ moves_loop:
 				&& !in_check
 				&& depth < 4
 				&& isQuiet
-				&& (quiet_moves.count > (depth * 8))) {
+				&& (quiet_moves.count > (4 + depth * depth))) {
 				SkipQuiets = true;
 				continue;
 			}
