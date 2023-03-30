9b6515d1f42b96688c8fa9a0fdc037404c8de50c
PGG106 <g.pagano45@studenti.unisa.it>
2023-03-30 06:01:20 +0200

    Bench: 3621635

--- src/eval.cpp
+++ src/eval.cpp
@@ -2,15 +2,7 @@
 
 // if we don't have enough material to mate consider the position a draw
 bool MaterialDraw(const S_Board* pos) {
-	//If we only have kings on the board then it's a draw
-	if (CountBits(Occupancy(pos, BOTH)) == 2)
-		return true;
-	//KN v K, KB v K
-	else if (CountBits(Occupancy(pos, BOTH)) == 3 && ((CountBits(GetPieceBB(pos, KNIGHT)) == 1) || (CountBits(GetPieceBB(pos, BISHOP)) == 1)))
-		return true;
-	//KNN v K, KN v KN
-	else if (CountBits(Occupancy(pos, BOTH)) == 4 && ((CountBits(GetPieceBB(pos, KNIGHT)) == 2)))
-		return true;
+
 	return false;
 }
 
