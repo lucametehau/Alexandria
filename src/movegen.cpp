c0b80ebfdc659623e9949a1a7f285de47765c76a
PGG106 <g.pagano45@studenti.unisa.it>
2023-04-06 19:01:48 +0200

    Bench: 3175821

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -71,8 +71,8 @@ static inline void AddPawnMove(const S_Board* pos, const int from, const int to,
 		if (from >= a7 &&
 			from <= h7) { // if the piece is moving from the 7th to the 8th rank
 			AddMove(encode_move(from, to, WP, WQ, capture), list);
-			AddMove(encode_move(from, to, WP, WR, capture), list); // consider every possible piece promotion
-			AddMove(encode_move(from, to, WP, WB, capture), list);
+			//AddMove(encode_move(from, to, WP, WR, capture), list); // consider every possible piece promotion
+			//AddMove(encode_move(from, to, WP, WB, capture), list);
 			AddMove(encode_move(from, to, WP, WN, capture), list);
 		}
 		else { // else do not include possible promotions
@@ -84,8 +84,8 @@ static inline void AddPawnMove(const S_Board* pos, const int from, const int to,
 		if (from >= a2 &&
 			from <= h2) { // if the piece is moving from the 2nd to the 1st rank
 			AddMove(encode_move(from, to, BP, BQ, capture), list);
-			AddMove(encode_move(from, to, BP, BR, capture), list); // consider every possible piece promotion
-			AddMove(encode_move(from, to, BP, BB, capture), list);
+			//AddMove(encode_move(from, to, BP, BR, capture), list); // consider every possible piece promotion
+			//AddMove(encode_move(from, to, BP, BB, capture), list);
 			AddMove(encode_move(from, to, BP, BN, capture), list);
 		}
 		else { // else do not include possible promotions
